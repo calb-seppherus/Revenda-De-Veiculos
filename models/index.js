5ed22aa581858fd84a1d17f17927c7eabe10078b
lucas <gabriel.freitas2006@proton.me>
2024-09-27 11:21:34 -0300

    criado as associações

--- models/index.js
+++ models/index.js
@@ -16,6 +16,14 @@ db.sequelize = sequelize;
 
 db.clientes = require("./cliente.model.js")(sequelize, Sequelize);
 db.veiculos = require("./veiculo.model.js")(sequelize, Sequelize);
-db.clientes = require("./vendedor.model.js")(sequelize, Sequelize);
+db.vendedores = require("./vendedor.model.js")(sequelize, Sequelize);
+
+//Associação entre cliente e veiculo
+db.clientes.hasMany(db.veiculos);
+db.veiculos.belongsTo(db.clientes);
+
+//Associação entre vendedor e veiculo
+db.vendedores.hasMany(db.veiculos);
+db.veiculos.belongsTo(db.vendedores);
 
 module.exports = db;
