46510933ea9eaf2b39a61a9488e22f47b6121015
lucas <gabriel.freitas2006@proton.me>
2024-09-30 12:01:25 -0300

    adicionado multer, rotas necessarias para funcionalidade adicionadas

--- routes/cliente.routes.js
+++ routes/cliente.routes.js
@@ -1,4 +1,40 @@
 module.exports = (app) => {
+
+    const multer = require("multer");
+    const fs = require("fs");
+    var path = require("path");
+    
+
+    var storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "uploads/cliente");
+        },
+        filename: function(req, file, cb) {
+            cb(null, Date.now() + path.extname(file.originalname));
+        }
+    });
+
+    const upload = multer({
+        storage: storage,
+    });
+
+    router.post("/upload/", upload.single("file"), async (req,res) => {
+        res.send({
+            upload: true,
+            file: req.file
+        });
+    });
+
+    router.get("/upload/:arquivo", (req,res) => {
+        const arquivo = path.dirname(__dirname)
+        + `/uploads/cliente/${req.params.arquivo}`;
+        fs.readFile(arquivo, function (err, data) {
+            res.contentType("png");
+            res.send(data);
+        });
+    });
+
+
     const clientes = require ("../controllers/cliente.controller.js");
     var router = require ("express").Router();
 
--- routes/vendedor.routes.js
+++ routes/vendedor.routes.js
@@ -1,4 +1,38 @@
 module.exports = (app) => {
+
+    const multer = require("multer");
+    const fs = require("fs");
+    var path = require("path");
+    
+    var storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "uploads/vendedor");
+        },
+        filename: function(req, file, cb) {
+            cb(null, Date.now() + path.extname(file.originalname));
+        }
+    });
+
+    const upload = multer({
+        storage: storage,
+    });
+
+    router.post("/upload/", upload.single("file"), async (req,res) => {
+        res.send({
+            upload: true,
+            file: req.file
+        });
+    });
+
+    router.get("/upload/:arquivo", (req,res) => {
+        const arquivo = path.dirname(__dirname)
+        + `/uploads/vendedor/${req.params.arquivo}`;
+        fs.readFile(arquivo, function (err, data) {
+            res.contentType("png");
+            res.send(data);
+        });
+    });
+
     const vendedores = require ("../controllers/vendedor.controller.js");
     var router = require ("express").Router();
 
